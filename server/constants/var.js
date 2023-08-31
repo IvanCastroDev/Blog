1c296338cf1744683b3e362b9e4d3a6a0773d055
IvanCastro19 <79170316+IvanCastro19@users.noreply.github.com>
2023-08-30 23:28:54 -0600

    udpate env file

--- server/constants/var.js
+++ server/constants/var.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const username = process.env.DB_USERNAME || "username";
-const password = process.env.DB_PASSWORD;
+const password = process.env.DB_PASSWORD || "password";
+const db = process.env.DB || "mongoDB";
 
 const constants = {
-    MONGO_URL: `mongodb+srv://${username}:${password}@cluster0.fd0fgzn.mongodb.net/?retryWrites=true&w=majority`
+    MONGO_URL: `mongodb+srv://${username}:${password}@${db}.fd0fgzn.mongodb.net/?retryWrites=true&w=majority`
 }
 
 export default constants;
