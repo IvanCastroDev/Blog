844685ed0971bc1ce8c79c5c3d9a1c7f57f64cf4
IvanCastro19 <79170316+IvanCastro19@users.noreply.github.com>
2023-08-30 23:41:53 -0600

    update env and gitignore files

--- server/constants/var.js
+++ server/constants/var.js
@@ -4,9 +4,10 @@ dotenv.config();
 
 const username = process.env.DB_USERNAME || "username";
 const password = process.env.DB_PASSWORD;
+const db =  process.env.DB;
 
 const constants = {
-    MONGO_URL: `mongodb+srv://${username}:${password}@cluster0.fd0fgzn.mongodb.net/?retryWrites=true&w=majority`
+    MONGO_URL: `mongodb+srv://${username}:${password}@${db}.fd0fgzn.mongodb.net/?retryWrites=true&w=majority`
 }
 
 export default constants;
